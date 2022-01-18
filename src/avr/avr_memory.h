5d9c216b0aa9a21fad3fa4a848d8de7c4ff4fd27
amanuellperez <amanuel.lperez@gmail.com>
2022-01-18 18:05:04 +0100

    Support for PROGMEM v0.0

--- src/avr/avr_memory.h
+++ src/avr/avr_memory.h
@@ -21,6 +21,22 @@
 
 #ifndef __AVR_MEMORY_H__
 #define __AVR_MEMORY_H__
+/****************************************************************************
+// *
+ *  - DESCRIPCION: Funciones para acceder a la memoria: RAM, flash, eeprom
+ *
+ *  - HISTORIA:
+ *    A.Manuel L.Perez
+ *    16/01/2022 v0.0
+ *
+ ****************************************************************************/
+#include <avr/pgmspace.h>   
+
+#include <type_traits>
+#include <initializer_list>
+#include <atd_type_traits.h>
+
+namespace avr{
 
 // Indica el número de bytes de RAM libres (para ver si se está usando
 // demasiada memoria).
@@ -35,7 +51,98 @@ inline int bytes_of_free_ram()
   return (int) &v - (__brkval == 0 ? (int) &__heap_start : (int) __brkval); 
 }
 
+/***************************************************************************
+ *				PROGMEM
+ ***************************************************************************/
+template <typename Int>
+class Progmem{
+public:
+    // TODO: implementar otros tipos (lo marca la función pgm_read_xxx)
+    static_assert(std::is_same_v<Int, uint8_t> or std::is_same_v<Int, uint16_t>);
 
+    constexpr Progmem(const Int& x): x_{x} { }
+    operator Int() const
+    {   
+	if constexpr (std::is_same_v<Int, uint8_t>)
+	    return pgm_read_byte(&x_);
+
+	else if constexpr (std::is_same_v<Int, uint16_t>)
+	    return pgm_read_word(&x_);
+
+	else
+	    static_assert(atd::always_false_v<int>, "error");
+
+    }
+
+    template<typename Int2>
+    Progmem& operator=(const Int2&) = delete;
+
+private:
+    const Int x_;
+};
+
+
+template <typename T, size_t N>
+class Progmem_array{
+public:
+    using size_type = size_t;
+
+
+    // TODO: implementar otros tipos
+    static_assert(std::is_same_v<T, uint8_t> or std::is_same_v<T, uint16_t>);
+
+    constexpr size_type size() const noexcept 
+    {return N;}
+
+    T operator[](size_type i) const
+    {   
+	if constexpr (std::is_same_v<T, uint8_t>)
+	    return pgm_read_byte(&data[i]);
+
+	else if constexpr (std::is_same_v<T, uint16_t>)
+	    return pgm_read_word(&data[i]);
+
+	else
+	    static_assert(atd::always_false_v<int>, "error");
+
+    }
+
+    template<typename T2>
+    Progmem_array& operator=(const T2&) = delete;
+
+    // Al principio iba a definir data como 'private'. Sin embargo, si se
+    // define como private da error al compilar (ya que no inicializa por
+    // defecto el data). Definiéndolo como const evito el problema de
+    // escritura. El usuario tiene que saber que no debe de leer directamente
+    // 'data'.
+    const T data[N];
+};
+
+
+
+// Para manejar arrays de cadenas.
+template <size_t N>
+class Progmem_string_array{
+public:
+    using size_type = size_t;
+
+    constexpr size_type size() const noexcept 
+    {return N;}
+
+    template<typename T2>
+    Progmem_string_array& operator=(const T2&) = delete;
+
+    char* strcpy(char* dst, size_type i) const
+    { return strcpy_P(dst, (PGM_P) pgm_read_word(&(data[i]))); }
+
+    const char* const data[N];
+
+};
+
+
+
+
+}// namespace
 #endif
 
 
---
+++ src/avr/test/memory/main.cpp
@@ -0,0 +1,142 @@
+// Copyright (C) 2022 A.Manuel L.Perez 
+//           mail: <amanuel.lperez@gmail.com>
+//           https://github.com/amanuellperez/mcu
+//
+// This file is part of the MCU++ Library.
+//
+// MCU++ Library is a free library: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+
+#include "../../avr_time.h"
+#include "../../avr_UART_iostream.h"
+#include "../../avr_memory.h"
+
+/***************************************************************************
+ *			    TEST BÁSICO 
+ ***************************************************************************/
+// tipos básicos
+constexpr uint8_t u8 PROGMEM = 14;
+constexpr uint16_t u16 PROGMEM = 27;
+
+// arrays de cadenas
+const char str1[] PROGMEM = "one";
+const char str2[] PROGMEM = "two";
+const char str3[] PROGMEM = "three";
+const char* const str_array[] PROGMEM = {
+    str1, str2, str3
+};
+
+
+// arrays en general
+constexpr uint8_t array_u8[] PROGMEM = {10,20,30,40};
+
+
+void test_basic()
+{
+    avr::UART_iostream uart;
+
+    while(1){
+    // Tipos básicos
+    {
+	uint8_t x8 = pgm_read_byte(&u8);
+	uart << "u8 = [" << (int) x8 << "]\n";
+
+	uint16_t x16 = pgm_read_word(&u16);
+	uart << "u16 = [" << x16 << "]\n";
+    }
+
+    // Arrays de cadenas
+    {
+	char buffer[10];
+	for (uint8_t i = 0; i < 3; ++i){
+	    strcpy_P(buffer, (PGM_P) pgm_read_word(&(str_array[i])));
+	    uart << "str[" << (int) i << "] = " << buffer << '\n';
+	}
+    }
+
+    // Arrays
+    {
+	for (uint8_t i = 0; i < 4; ++i){
+	    uint8_t x8 = pgm_read_byte(&array_u8[i]);
+	    uart << "u8 = [" << (int) x8 << "]\n";
+	}
+    }
+
+
+	wait_ms(1000);
+    }
+
+}
+
+
+
+constexpr avr::Progmem<uint8_t> pu8 PROGMEM = 12;
+constexpr avr::Progmem<uint16_t> pu16 PROGMEM = 200;
+// Este no tiene que compilar:
+// constexpr Progmem<uint32_t> pu32 PROGMEM = 200;
+
+constexpr avr::Progmem_array<uint8_t, 4> parray_u8 PROGMEM = {10,20,30,40};
+constexpr avr::Progmem_string_array<3> parray_str PROGMEM = {
+    str1, str2, str3
+    };
+
+void test_progmem()
+{
+    avr::UART_iostream uart;
+
+    while(1){
+	uart << "\n\n";
+	uint8_t x8 = pu8;
+	uart << "u8 = [" << (int) x8 << "]\n";
+	
+//	pu8 = 7;  // no compila
+
+    {// Arrays
+	uint16_t x16 = pu16;
+	uart << "u16 = [" << (int) x16 << "]\n";
+
+	for (size_t i = 0; i < parray_u8.size(); ++i)
+	    uart << "u8[" << i << "] = " << (int) parray_u8[i] << '\n';
+    }
+	
+    uart << "Escribiendo array de " << parray_str.size() << " cadenas:\n";
+    // Arrays de cadenas
+    {
+	char buffer[10];
+	for (size_t i = 0; i < parray_str.size(); ++i){
+//	    avr::strcpy(buffer, parray_str[i]);
+	    parray_str.strcpy(buffer, i);
+	    uart << "str[" << (int) i << "] = " << buffer << '\n';
+	}
+    }
+	wait_ms(1000);
+    }
+
+}
+
+
+int main()
+{
+// init_UART();
+    avr::UART_iostream uart;
+    avr::basic_cfg(uart);
+    uart.on();
+
+    uart << "\nProgmem test\n"
+	      "------------\n";
+
+   //test_basic();
+   test_progmem();
+}
+
