d5f7cedb89c0c05b5e2492e604123ac1c4919d70
amanuellperez <prog154cpp@proton.me>
2023-09-28 15:01:57 +0200

    Improving std::char_traits

--- src/std/pc_test/char_traits/main.cpp
+++ src/std/pc_test/char_traits/main.cpp
@@ -17,23 +17,23 @@
 // You should have received a copy of the GNU General Public License
 // along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
-#include "../../std_char_traits.h"
+#include "../../std_char_traits.h" 
 
 #include <alp_test.h>
+#include <alp_string.h>
+
 #include <iostream>
 #include <vector>
 #include <cstring>
 
-using namespace std;
 using namespace test;
 
-
-
-void test_char_traits()
+void test_basic()
 {
-    using tr = char_traits<char>;
+    using tr = mtd::char_traits<char>;
     
-    test::interfaz("char_traits");
+    test::interface("basic");
+
     {
 	tr::char_type a, b;
 	a = 'a';
@@ -67,15 +67,112 @@ void test_char_traits()
     }
 }
 
+
+void test_length(const char* str)
+{
+    using mct = mtd::char_traits<char>;
+    using sct = std::char_traits<char>;
+    CHECK_TRUE(mct::length(str) == sct::length(str), 
+		alp::as_str() << "length(" << str << ")");
+}
+
+void test_length()
+{
+    test::interface("length");
+
+    test_length("");
+    test_length("abc");
+    
+}
+
+bool equal_signs(int x, int y)
+{
+    if (x== 0){
+	if (y == 0)	return true;
+	else		return false;
+    }
+    else if (x > 0){
+	if (y > 0)	return true;
+	else		return false;
+    }
+    else{ // x < 0
+	if (y < 0)	return true;
+	else		return false;
+    }
+// para ver la segunda parte: return (x*y > 0);
+}
+
+void test_compare(const char* s1, const char* s2, size_t n)
+{
+    using mct = mtd::char_traits<char>;
+    using sct = std::char_traits<char>;
+
+    CHECK_TRUE(equal_signs(mct::compare(s1, s2, n), sct::compare(s1, s2, n)), 
+		alp::as_str() << "compare(" << s1 << ", " << s2 
+			      << ", " << n << ")");
+}
+
+void test_compare()
+{
+    test::interface("compare");
+
+    test_compare("", "", 0);
+
+    test_compare("abc", "abc", 0);
+    test_compare("abc", "abc", 1);
+    test_compare("abc", "abc", 2);
+    test_compare("abc", "abc", 3);
+
+    test_compare("abcde", "abcjk", 0);
+    test_compare("abcde", "abcjk", 1);
+    test_compare("abcde", "abcjk", 2);
+    test_compare("abcde", "abcjk", 3);
+    test_compare("abcde", "abcjk", 4);
+    test_compare("abcde", "abcjk", 5);
+}
+
+void test_find(const char* s1, size_t n, char c)
+{
+    using mct = mtd::char_traits<char>;
+    using sct = std::char_traits<char>;
+
+    CHECK_TRUE(mct::find(s1, n, c) == sct::find(s1, n, c), 
+		alp::as_str() << "find(" << s1 << ", " << n
+			      << ", " << c << ")");
+}
+void test_find()
+{
+    test::interface("find");
+    
+    test_find("", 0, 'd');
+
+    test_find("a", 1, 'a');
+    test_find("a", 1, 'b');
+
+    test_find("abcdefg", 0, 'd');
+    test_find("abcdefg", 1, 'd');
+    test_find("abcdefg", 2, 'd');
+    test_find("abcdefg", 3, 'd');
+    test_find("abcdefg", 4, 'd');
+    test_find("abcdefg", 5, 'd');
+    test_find("abcdefg", 6, 'd');
+    test_find("abcdefg", 7, 'd');
+
+}
 int main()
 {
 try{
     test::header("char_traits");
 
-    test_char_traits();
+    test_basic();
 
-}catch(alp::Excepcion& e){
-    cerr << e.what() << '\n';
+    test_compare();
+    test_length();
+    test_find();
+
+
+}catch(const std::exception& e){
+    std::cerr << e.what() << '\n';
     return 1;
 }
 
